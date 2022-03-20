f4eac1db10961ec8349f2c7d3effcbd4b971d82d
Rio0628 <mariomdomenech@gmail.com>
2022-03-20 17:07:50 -0400

    Created the sidebar view for application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { BsArrowBarLeft } from 'react-icons/bs';
 
 class App extends Component {
   render () {
@@ -17,10 +18,56 @@ class App extends Component {
           </div>
 
         </div>
+
+        <div className='sidebar'>
+
+          <div className='logoReturnBtnCntr'>
+            <p className='logoApp'>Exercise Pedia</p>
+            
+            <div className='returnBtn'><BsArrowBarLeft className='logo'/></div>
+          </div>
+
+          <p className='bodypartBtn'>Bodyparts</p>
+
+            <div className='itemsViewCntr'>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+            </div>
+
+          <p className='targetareasBtn'>Target Areas</p>
+
+            <div className='itemsViewCntr'>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+            </div>
+
+          <p className='equipmentBtn'>Equipment</p>
+
+            <div className='itemsViewCntr'>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+            </div>
+
+          <p className='savedExercisesBtn'>Saved Exercises</p>
+
+            <div className='itemsViewCntr'>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+              <p className='indItem'>Item</p>
+            </div>
+        </div>
         
         <div className='infoCntr'>
 
         </div>
+
       </div>
     );
   }
