bea77785c0e235cab9c7b4b79498f76006de1049
Rio0628 <mariomdomenech@gmail.com>
2022-03-20 11:41:47 -0400

    Created navbar elmnts and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 class App extends Component {
   render () {
     return (
       <div className="container">
-        <h1>HELLOOOOO</h1>
+        
+        <div className='nav-bar'>
+          <div className='menuBtn'><GiHamburgerMenu className='icon'/></div>
+        
+          <div className='searchbarCntr'>
+            <input className='searchbar' type='text' placeholder='Search Exercise'/>
+
+            <div className='searchBtn'><AiOutlineSearch className='logo'/></div>
+          </div>
+
+        </div>
+        
+        <div className='infoCntr'>
+
+        </div>
       </div>
     );
   }
