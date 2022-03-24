f6c6d6092fcff0f4cbc92c816d7d93f2ad056099
Rio0628 <mariomdomenech@gmail.com>
2022-03-24 16:28:19 -0400

    Created sidebar anims and added concurrently login

--- src/App.js
+++ src/App.js
@@ -3,9 +3,43 @@ import { AiOutlineSearch } from 'react-icons/ai';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { BsArrowBarLeft } from 'react-icons/bs';
 import { SearchRslts, IndExercise, SavedCategory } from './components';
+import axios from 'axios';
+import gsap from 'gsap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "mario"
+    };
+    this.sidebarRef = React.createRef();
+    this.sidebarAnim = gsap.timeline({ paused: true });
+  }
+
+
+  componentDidMount() {
+    // Sidebar appear anim
+    this.sidebarAnim.to(this.sidebarRef.current, { opacity: 1, x: 0, duration: 1, ease: 'none'} )
+  }
+
+  
   render () {
+
+    // let options = {
+    //   method: 'GET',
+    //   url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    //   headers: {
+    //     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    //     'X-RapidAPI-Key': 'b3a01735bbmshff0db8d8ae4e31dp101e3cjsnce8522bd9a34'
+    //   }
+    // };
+
+    // axios.request(options).then(function (response) {
+    //   console.log(response.data);
+    // }).catch(function (error) {
+    //   console.error(error);
+    // });
+
     return (
       <div className="container">
         
@@ -13,7 +47,7 @@ class App extends Component {
 
           <p className='logoApp'>Exercise Pedia</p>
 
-          <div className='menuBtn'><GiHamburgerMenu className='icon'/></div>
+          <div className='menuBtn' onClick={() => this.sidebarAnim.play()} ><GiHamburgerMenu className='icon'/></div>
         
           <div className='searchbarCntr'>
             <input className='searchbar' type='text' placeholder='Search Exercise'/>
@@ -26,12 +60,12 @@ class App extends Component {
         <div className='mainCntr'>
 
           
-          <div className='sidebar'>
+          <div className='sidebar' ref={this.sidebarRef}>
 
             <div className='logoReturnBtnCntr'>
               <p className='logoApp'>Exercise Pedia</p>
             
-              <div className='returnBtn'><BsArrowBarLeft className='logo'/></div>
+              <div className='returnBtn' onClick={() => this.sidebarAnim.reverse()} ><BsArrowBarLeft className='logo'/></div>
             </div>
 
             <p className='bodypartBtn'>Bodyparts</p>
