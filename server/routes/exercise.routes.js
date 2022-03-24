6e0ed8d27326f8f6a6779d9eba485e7edc3ee316
Rio0628 <mariomdomenech@gmail.com>
2022-03-24 11:40:22 -0400

    Created Route for app

---
+++ server/routes/exercise.routes.js
@@ -0,0 +1,9 @@
+module.exports = app => {
+    const exercises = require('../controllers/exercise.controller');
+    let router = require('express').Router();
+
+    router.post('/', exercises.create);
+    router.get('/', exercises.findAll);
+    router.put('/:id', exercises.update);
+    router.delete('/:id', exercises.delete);
+}
