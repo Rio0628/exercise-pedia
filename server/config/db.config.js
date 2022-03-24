749edad88264a2ca980937e268d2862f8f349f6a
Rio0628 <mariomdomenech@gmail.com>
2022-03-23 21:19:15 -0400

    Added main index server file and db config file

---
+++ server/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "bdda90cc548d57",
+    PASSWORD: "843f0fb7",
+    DB: "heroku_2ad03bec26efca8",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
