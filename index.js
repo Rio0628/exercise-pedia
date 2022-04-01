8d3457e2113d98577c03ddd64b022e39114dbf7c
Rio0628 <mariomdomenech@gmail.com>
2022-04-01 13:45:37 -0400

    Added heroku functionality

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const db = require('./server/models');
+const buildPath = path.join(__dirname, 'build');
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-db.sequelize.sync();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(buildPath));
+    app.get('/', (req, res) => { res.sendFile(path.resolve(buildPath, 'index.html')) });
+} else { 
+    app.get('/', (req, res) => { res.json({ message: 'Hello World!' }) });
+}
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
-});
+db.sequelize.sync();
 
 require('./server/routes/exercise.routes')(app);
 require('./server/routes/category.routes')(app);
